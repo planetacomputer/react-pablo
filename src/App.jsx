bae1eeb5eddefb6bda5c1f306c1e1f1076a0870b
David Casanovas Osorio <dcasanovaso@ioc.cat>
2023-11-25 17:13:39 +0100

    15

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
 import { useRef } from 'react'
 const App = () => {
-  const gradosRef = useRef();
-  const fahrenheitRef = useRef();
-
-  const convertirGradosFahrenheit = () => {
-    fahrenheitRef.current.innerHTML = ((gradosRef.current.value*9/5)+32);
+  const anchoRef = useRef();
+  const altoRef = useRef();
+  const superficieRef = useRef();
+  const calcularSuperficie = () => {
+    superficieRef.current.innerHTML = anchoRef.current.value * altoRef.current.value;
   }
+  
   return (
     <div className='w3-container w3-khaki'>
-    <input type="text" ref={gradosRef} />
-    <button onClick={convertirGradosFahrenheit}>Convertir</button>
-    <div className='w3-badge w3-sand' ref={fahrenheitRef}></div>
+    <input type="text" ref={anchoRef} />
+    <input type="text" ref={altoRef} />
+    <button onClick={calcularSuperficie}>Calcular</button>
+    <div className='w3-badge w3-sand' ref={superficieRef}></div>
     </div>
   )
 }
