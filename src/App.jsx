a4fccdbd5a4b357adb5cb837c7a4fc702ca1f2ea
David Casanovas Osorio <dcasanovaso@ioc.cat>
2023-11-25 16:41:52 +0100

    11

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
 import { useState } from 'react'
-const App = () => <div>hola</div> 
-
+const App = () => {
+  const element = <div>Hello World</div>
+  function multiplicar (a, b) {
+    return a * b
+  }
+  const JSX = (
+    <div className='nombre'>
+      <h1>Add a class to this div</h1>
+      {/* comentarios */}
+    </div>
+  )
+  const producto = multiplicar(2, 3)
+  const numeros = [1, 2, 3, 4, 5]
+  return (
+    <>
+    <div>{element} {JSX} {producto}</div>
+    <div><button onClick={() => alert('Pulsado')}>Click me</button></div>
+    <div>
+      <ul>
+        {numeros.map((n, key) => <li key={key}>{n}</li>)}
+      </ul>
+    </div>
+    </>
+  )
+}
 export default App
