4592846046036e46855137333d9ba3c4ac250998
David Casanovas Osorio <dcasanovaso@ioc.cat>
2023-11-25 16:47:39 +0100

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 const App = () => {
-  const element = <div>Hello World</div>
-  function multiplicar (a, b) {
-    return a * b
+  const nombreRef = useRef();
+  const resultadoRef = useRef();
+
+  const procesar = () => {
+    resultadoRef.current.innerHTML = nombreRef.current.value;
   }
-  const JSX = (
-    <div className='nombre'>
-      <h1>Add a class to this div</h1>
-      {/* comentarios */}
-    </div>
-  )
-  const producto = multiplicar(2, 3)
-  const numeros = [1, 2, 3, 4, 5]
   return (
     <>
-    <div>{element} {JSX} {producto}</div>
-    <div><button onClick={() => alert('Pulsado')}>Click me</button></div>
-    <div>
-      <ul>
-        {numeros.map((n, key) => <li key={key}>{n}</li>)}
-      </ul>
-    </div>
+    <input type="text" ref={nombreRef} />
+    <button onClick={procesar}>Procesar</button>
+    <div ref={resultadoRef}></div>
     </>
   )
 }
