7cd8ef8fa6825aaa7bcc251d5ebeeb91682ae6df
David Casanovas Osorio <dcasanovaso@ioc.cat>
2024-01-13 16:24:39 +0100

    18

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
 import { useRef } from 'react'
-import { useState } from 'react';
+import { useState } from 'react'
 
 const App = () => {
-  const eurosRef = useRef();
-  const [dolares, setDolares] = useState(0);
+  const gradosRef = useRef();
+  const [fahrenheit, setFahrenheit] = useState(0);
 
-  const convertirEuroDolar = () => {
-    setDolares(eurosRef.current.value*2);
+  const convertirGradosFahrenheit = () => {
+    setFahrenheit((gradosRef.current.value*9/5)+32);
   }
   return (
     <div className='w3-container w3-khaki'>
-    <input type="text" ref={eurosRef} />
-    <button onClick={convertirEuroDolar}>Convertir</button>
-    <div className='w3-badge w3-sand'>{dolares}</div>
+    <input type="text" ref={gradosRef} />
+    <button onClick={convertirGradosFahrenheit}>Convertir</button>
+    <div className='w3-badge w3-sand'>{fahrenheit}</div>
     </div>
   )
 }
