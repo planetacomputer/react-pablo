aa7cecba627be6faf3eafefa6f96322848661b71
David Casanovas Osorio <dcasanovaso@ioc.cat>
2024-01-20 18:37:51 +0100

    19

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import './App.css'
-import { useState } from 'react'
-
+import { useRef, useState } from 'react'
 const App = () => {
-  const [image, setImage] = useState(0);
-  const ANIMAL_IMAGES = [
-    'http://via.placeholder.com/100x100',
-    'http://via.placeholder.com/200x200',
-    'http://via.placeholder.com/300x300'
-  ]
-  const placeImage = (number) => {
-    setImage(number);
+  const anchoRef = useRef();
+  const altoRef = useRef();
+  const [superficie, setSuperficie] = useState(0);
+
+  const calcularSuperficie = () => {
+    setSuperficie(anchoRef.current.value * altoRef.current.value);
   }
+  
   return (
     <div className='w3-container w3-khaki'>
-    <button onClick={()=>{placeImage(0)}}>Image 1</button>
-    <button onClick={()=>{placeImage(1)}}>Image 2</button>
-    <button onClick={()=>{placeImage(2)}}>Image 3</button>
-    <img src={ANIMAL_IMAGES[image]} alt='Animal' />
-    <div>{image+1}</div>
+    <input type="text" ref={anchoRef} />
+    <input type="text" ref={altoRef} />
+    <button onClick={calcularSuperficie}>Calcular</button>
+    <div className='w3-badge w3-sand'>{superficie}</div>
     </div>
   )
 }
